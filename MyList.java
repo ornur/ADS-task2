d776f56d57c9588de472dd8e4fd37807e0dad20f
Nurda <83532144+ornur@users.noreply.github.com>
2023-04-15 20:14:51 +0600

    Add files via upload

--- MyList.java
+++ MyList.java
@@ -1,6 +1,15 @@
 package org.example;
 
-public interface MyList {
-    private int size;
-
+public interface MyList<T> {
+    int size();
+    boolean contains(Object O);
+    void add(T item);
+    void add (T item, int index);
+    boolean remove(T item);
+    T remove(int index);
+    void clear ();
+    T get(int index);
+    int indexOf(Object o);
+    int lastIndexOf(Object o);
+    void sort();
 }
